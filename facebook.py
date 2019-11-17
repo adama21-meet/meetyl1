c3e17d7986d23435e412be00ba8009cf16d45c15
adama21-meet <52905045+adama21-meet@users.noreply.github.com>
2019-11-17 18:50:22 +0200

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -6,7 +6,6 @@ class User():
 		self.friends_list=[]
 		self.posts=[]
 
-
 	def add_friend(self,email):
 		self.friends_list.append(email)
 		print("has added "+email+' as a friend')
@@ -15,12 +14,37 @@ class User():
 		self.friends_list.remove(email)
 		print("has added "+email+' as a friend')
 
-	def add_post(self,text):
-		self.posts.append(text)
-		print(self.name+' has posted '+text)
+	def add_post(self,post_text,date):
+		self.date=date
+		self.text=post_text
+		author=self.name
+		post1=Post(date,post_text)
+		self.posts.append(post1)
+		print(self.name+' has posted '+post_text)
 
 	def get_userinfo(self):
-		print('name' + self.name + '\nemail' + self.email + '\npassword' + self.password + '\nfriends' + self.friends_list + '\nposts' + self.posts)
+		print('name: ' + self.name + ' email: ' + self.email + ' password: ' + self.password + ' friends: ' + str(self.friends_list) + 'posts' + str(self.posts))
+
+
+class Post(User):
+	def __init__(self,post_text,date):
+		self.post_text=post_text
+		self.date=date
+		self.comments=[]
+
+		def create_comment(self,comment_text):
+			self.comments.append(comment_text)
+			print('your comment is ' + self.comment_text)
+
+		def remove_comment(self):
+			self.comments.remove(comment_text)
+
+		def post_date(self):
+			print('posted on ' + self.date)
+
+		def edit_comment(self,new_comment):
+			self.comment_text=new_comment
+			print('your comment has been changed!')
 
 
 
@@ -32,7 +56,7 @@ user2 = User('sarah','sarah@gmail.com', 'sarah2003')
 user1.add_friend(user2.email)
 user2.add_friend(user1.email)
 
-user1.add_post('thats my text 1')
-user2.add_post('that my text 2')
+user1.add_post('say hello','30/1')
+user2.add_post('that my text 2','4/4')
 
 user1.get_userinfo()
