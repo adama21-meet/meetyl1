c573c3e2e911ee111af2d15879795d36ca8a3296
adama21-meet <52905045+adama21-meet@users.noreply.github.com>
2019-11-20 20:00:55 +0200

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -1,10 +1,14 @@
+users=[]
+posts=[]
+
+
+
 class User():
 	def __init__(self,name,email,password):
 		self.name=name
 		self.email=email
 		self.password=password
 		self.friends_list=[]
-		self.posts=[]
 
 	def add_friend(self,email):
 		self.friends_list.append(email)
@@ -15,29 +19,30 @@ class User():
 		print("has added "+email+' as a friend')
 
 	def add_post(self,post_text,date):
-		self.date=date
-		self.text=post_text
+		self.post_text=post_text
 		author=self.name
 		post1=Post(date,post_text)
-		self.posts.append(post1)
+		posts.append(post1)
 		print(self.name+' has posted '+post_text)
+
+#	def create_comment(self,comment_text):
+#		self.comment_text=comment_text
+#		comment1=Post()
+#		self.comments.append(comment1)
 
 	def get_userinfo(self):
 		print('name: ' + self.name + ' email: ' + self.email + ' password: ' + self.password + ' friends: ' + str(self.friends_list) + 'posts' + str(self.posts))
 
 
-class Post(User):
+class Post():
 	def __init__(self,post_text,date):
 		self.post_text=post_text
 		self.date=date
 		self.comments=[]
 
-		def create_comment(self,comment_text):
-			self.comments.append(comment_text)
-			print('your comment is ' + self.comment_text)
+		def post(self):
+			posts.append(post_text)
 
-		def remove_comment(self):
-			self.comments.remove(comment_text)
 
 		def post_date(self):
 			print('posted on ' + self.date)
@@ -46,7 +51,16 @@ class Post(User):
 			self.comment_text=new_comment
 			print('your comment has been changed!')
 
+class comment(Post):
+	def __init__(self,comment_text):
+		self.comment_text=comment_text
 
+	def create_comment(self):
+			self.comments.append(comment_text)
+			print('your comment is ' + self.comment_text)
+
+	def remove_comment(self):
+			self.comments.remove(comment_text)
 
 user1 = User('adam','tsotzukth1@gmail.com','adam2004')
 
@@ -59,4 +73,5 @@ user2.add_friend(user1.email)
 user1.add_post('say hello','30/1')
 user2.add_post('that my text 2','4/4')
 
-user1.get_userinfo()
+user1.create_comment('comment')
+#user1.get_userinfo()
