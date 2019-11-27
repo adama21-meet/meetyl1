4273ec2c38dad2adacee7506a4e86accb44b62a1
adama21-meet <52905045+adama21-meet@users.noreply.github.com>
2019-11-27 18:43:37 +0200

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -8,8 +8,9 @@ password='123'
 random=0
 
 while random==0:
-	try_username=input('whats the username?')
-	try_password=input('whats the password?')
+	print('Welcome to facebook, please login to continue ')
+	try_username=input('whats the username? ')
+	try_password=input('whats the password? ')
 
 	if try_username==username and try_password==password:
 		random=1
@@ -24,6 +25,7 @@ class User():
 		self.email=email
 		self.password=password
 		self.friends_list=[]
+		self.new_msgs=[]
 		users.append(self)
 
 	def add_friend(self,email):
@@ -49,6 +51,14 @@ class User():
 	def get_userinfo(self):
 		print('name: ' + self.name + ' email: ' + self.email + ' password: ' + self.password + ' friends: ' + str(self.friends_list) + 'posts' + str(self.posts))
 
+	def send_dm(self):
+		reciver=input('how do you want to DM? ')
+		message_text=input('type in your message. ')
+		for reciver in users:
+			if reciver in self.name:
+				reciver.append(message_text)
+
+
 
 class Post():
 	def __init__(self,post_text,date):
@@ -62,10 +72,6 @@ class Post():
 		def post_date(self):
 			print('posted on ' + self.date)
 
-		def edit_comment(self,new_comment):
-			self.comment_text=new_comment
-			print('your comment has been changed!')
-
 
 class Comment(Post):
 	def __init__(self,comment_text):
@@ -76,23 +82,23 @@ class Comment(Post):
 		print('your comment is ' + self.comment_text)
 
 
+	def edit_comment(self,new_comment):
+		self.comment_text=new_comment
+		print('your comment has been changed!')
+
 
 class Messages(User):
 	def __init__(self,message_text):
 		self.message_text=message_text
-
-	reciver=input('who do you want to DM?')
-	for i in users:
-		if reciver==i.name
 
 
 
 
 user1 = User('adam','tsotzukth1@gmail.com','adam2004')
 
-user2 = User('sarah','sarah@gmail.com', 'sarah2003')
+user2 = User('Noa','noa@gmail.com', 'noaaaa200')
 
-
+'''
 user1.add_friend(user2.email)
 user2.add_friend(user1.email)
 
@@ -100,3 +106,5 @@ user1.add_post('say hello','30/1')
 user2.add_post('that my text 2','4/4')
 
 user1.create_comment('hello')
+'''
+
