6e20ff2edf91b98b3097070481a08c850d10483a
adama21-meet <52905045+adama21-meet@users.noreply.github.com>
2020-02-02 19:38:10 +0200

    Add files via upload

--- story.js
+++ story.js
@@ -1,11 +1,3 @@
-
-
-function Fun1() {
-  document.getElementById("text").innerHTML = "Good!";
-}
-
-
-
-function Fun2() {
-  document.getElementById("text").innerHTML = "Bad!";
+function repeat() {
+	window.open("story.html");
 }
